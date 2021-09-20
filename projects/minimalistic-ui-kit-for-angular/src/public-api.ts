6063801539595fe8a3e173950e10878fcd3aa460
Vladislav Peltikhin <peltikhin.vladik@gmail.com>
2021-09-20 17:18:31 +0300

    Пытаюсь починить git submodule, потому что он не работает(совсем)

--- projects/minimalistic-ui-kit-for-angular/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of minimalistic-ui-kit-for-angular
- */
-
-export * from './lib/minimalistic-ui-kit-for-angular.service';
-export * from './lib/minimalistic-ui-kit-for-angular.component';
-export * from './lib/minimalistic-ui-kit-for-angular.module';
